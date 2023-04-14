3304f856165add94a11423872c5b6de1c6e83b64
Eduardo <prog.syrius@gmail.com>
2023-04-14 10:22:21 -0300

    chore: add Tailwindcss, Eslint and Prettier config

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    screens: {
+      tablet: { max: '768px' },
+      // => @media (max-width: 768px) { ... }
+
+      smartphone: { max: '480px' }
+      // => @media (max-width: 480px) { ... }
+    },
+    extend: {
+      colors: {
+        lightGray: '#f1f1f1',
+        primary: '#202020'
+      },
+      fontFamily: {
+        sans: ['var(--font-poppins)', ...defaultTheme.fontFamily.sans],
+        serif: [
+          'var(--font-cormorant-garamond)',
+          ...defaultTheme.fontFamily.serif
+        ]
+      }
+    }
+  },
+  plugins: []
+}
