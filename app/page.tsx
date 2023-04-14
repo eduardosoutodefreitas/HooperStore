0bd4b02a2d875685eb4c3ba468851ce982802716
Eduardo <prog.syrius@gmail.com>
2023-04-14 20:49:05 -0300

    feat: add initial request to Firestore

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
+'use client'
+import { collection, getDocs } from 'firebase/firestore'
+import { useState, useEffect } from 'react'
+import { db } from './config/firebase.config'
 import Banner from './sections/Banner'
+import Categories from './sections/Categories'
+import Category from './types/Category.types'
 export default function Home() {
+  const [categories, setCategories] = useState<Category[]>([])
+  const fetchCategories = async () => {
+    try {
+      const categoriesFromFirestore: Category[] = []
+      const q = collection(db, 'categories')
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc) => {
+        categoriesFromFirestore.push(doc.data() as any)
+      })
+      setCategories(categoriesFromFirestore)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  console.log(categories)
   return (
     <main className="px-12 py-7">
       <Banner />
+      <Categories />
     </main>
   )
 }
