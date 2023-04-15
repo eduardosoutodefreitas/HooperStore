21926f8b75f6406a2a615f639dce4df34c13e18b
Eduardo <prog.syrius@gmail.com>
2023-04-15 18:29:15 -0300

    feat: add initial categories fetch method

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,15 @@
 'use client'
-import { collection, getDocs } from 'firebase/firestore'
-import { useState, useEffect } from 'react'
-import { db } from './config/firebase.config'
+
+import { useContext, useEffect } from 'react'
+import { CategoryContext } from './contexts/category.context'
 import Banner from './sections/Banner'
 import Categories from './sections/Categories'
-import Category from './types/Category.types'
-export default function Home() {
-  const [categories, setCategories] = useState<Category[]>([])
-  const fetchCategories = async () => {
-    try {
-      const categoriesFromFirestore: Category[] = []
-      const q = collection(db, 'categories')
-      const querySnapshot = await getDocs(q)
-      querySnapshot.forEach((doc) => {
-        categoriesFromFirestore.push(doc.data() as any)
-      })
-      setCategories(categoriesFromFirestore)
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
+export default function Home() {
+  const { fetchCategories } = useContext(CategoryContext)
   useEffect(() => {
     fetchCategories()
   }, [])
-
-  console.log(categories)
   return (
     <main className="px-12 py-7">
       <Banner />
