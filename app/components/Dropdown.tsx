ea5a56fee6564c0221246b76c880b5145671070f
Eduardo <prog.syrius@gmail.com>
2023-04-15 08:58:09 -0300

    style: change Dropdown Color and z-index

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -6,8 +6,8 @@ interface DropdownProps {
 const Dropdown = ({ options, active, classes }: DropdownProps) => {
   return (
     <div
-      className={` flex flex-col gap-x-9 px-4 py-2 bg-gray-100/50 rounded-md w-[110px] absolute 
-      -left-2.5 transition-all  ease-in duration-150
+      className={` flex flex-col gap-x-9 px-4 py-2 bg-white rounded-md w-[110px] absolute 
+      -left-2.5 transition-all  ease-in duration-150 z-30
       ${active ? 'opacity-100 top-8 ' : 'opacity-0 top-5'} ${classes}`}>
       {options.map((option: string) => (
         <a href="#" key={option} className={`font-light hover:font-normal`}>
