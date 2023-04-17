06015b7a80b4c1d6429f14766c55c077fe7f183f
Eduardo <prog.syrius@gmail.com>
2023-04-17 07:08:02 -0300

    feat: add CategoryCard to Categories Section

--- app/sections/Categories.tsx
+++ app/sections/Categories.tsx
@@ -1,15 +1,20 @@
-'use client'
+import { useContext } from 'react'
 import { SectionSubtitle, SectionTitle } from '../components/SpecialText'
-import { collection, getDocs } from 'firebase/firestore'
-import { db } from '../config/firebase.config'
-import Category from '../types/Category.types'
-import { useEffect, useState } from 'react'
+
+import { CategoryContext } from '../contexts/category.context'
+import CategoryCard from '../components/CategoryCard'
 
 export default function Categories() {
+  const { categories } = useContext(CategoryContext)
   return (
-    <section className="my-10 mt-16">
+    <section className="my-10 mt-16 ">
       <SectionTitle title="Categorias" />
       <SectionSubtitle subtitle="Explore nossos produtos por categoria" />
+      <div className="flex justify-center items-center gap-x-2 px-4">
+        {categories.map((category) => (
+          <CategoryCard category={category} />
+        ))}
+      </div>
     </section>
   )
 }
