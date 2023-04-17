af5ac28490726e54d93d942248e44c8500dc606c
Eduardo <prog.syrius@gmail.com>
2023-04-17 19:00:16 -0300

    feat: change data fetch method

--- app/sections/Categories.tsx
+++ app/sections/Categories.tsx
@@ -1,18 +1,34 @@
-import { useContext } from 'react'
 import { SectionSubtitle, SectionTitle } from '../components/SpecialText'
 
-import { CategoryContext } from '../contexts/category.context'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../config/firebase.config'
+import Category from '../types/Category.types'
 import CategoryCard from '../components/CategoryCard'
+async function getCategories() {
+  const categoriesFromFirestore: Category[] = []
+  try {
+    const q = collection(db, 'categories')
+    const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc: { data: () => any }) => {
+      categoriesFromFirestore.push(doc.data() as any)
+    })
+    console.log(categoriesFromFirestore)
+  } catch (error) {
+    console.log(error)
+  }
 
-export default function Categories() {
-  const { categories } = useContext(CategoryContext)
+  return categoriesFromFirestore
+}
+
+export default async function Categories() {
+  const categories = await getCategories()
   return (
     <section className="my-10 mt-16 ">
       <SectionTitle title="Categorias" />
       <SectionSubtitle subtitle="Explore nossos produtos por categoria" />
       <div className="flex justify-center items-center gap-x-2 px-4">
         {categories.map((category) => (
-          <CategoryCard category={category} />
+          <CategoryCard category={category} key={category.id} />
         ))}
       </div>
     </section>
