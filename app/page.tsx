b1201e1386cae5e10db582d60aac6f557e31731b
Eduardo <prog.syrius@gmail.com>
2023-04-17 09:47:03 -0300

    feat: add Services Section to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useContext, useEffect } from 'react'
 import { CategoryContext } from './contexts/category.context'
 import Banner from './sections/Banner'
 import Categories from './sections/Categories'
+import Services from './sections/Services'
 
 export default function Home() {
   const { fetchCategories } = useContext(CategoryContext)
@@ -14,6 +15,7 @@ export default function Home() {
     <main className="px-12 py-7">
       <Banner />
       <Categories />
+      <Services />
     </main>
   )
 }
