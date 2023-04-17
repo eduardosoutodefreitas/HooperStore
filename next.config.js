dce0e52857d5d87f135c314d61e362dbe70938fd
Eduardo <prog.syrius@gmail.com>
2023-04-17 07:06:03 -0300

    chore: add remotePatterns from categories images to next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
