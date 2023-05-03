f52f24b4b0d0ea20492f7fff011d81f5f1c4d8c4
Edu <prog.syrius@gmail.com>
2023-05-03 19:57:01 -0300

    feat: add Services Section and Service Component

---
+++ src/app/sections/Services.tsx
@@ -0,0 +1,34 @@
+import Service from '../components/Service'
+import { BiSupport } from 'react-icons/bi'
+import {
+  TbTruckDelivery,
+  TbShoppingCartDiscount,
+  TbReceiptRefund
+} from 'react-icons/tb'
+
+export default function Services() {
+  return (
+    <section className="my-16 flex justify-between items-center p-5 py-7 bg-primary/5">
+      <Service
+        icon={<BiSupport size={55} />}
+        title="Suporte"
+        description="Atendimento ágil 24/7"
+      />
+      <Service
+        icon={<TbTruckDelivery size={55} />}
+        title="Entrega"
+        description="Frete grátis em compras acima R$100,00"
+      />
+      <Service
+        icon={<TbShoppingCartDiscount size={55} />}
+        title="Descontos"
+        description="Ofertas diárias de até 70% off"
+      />
+      <Service
+        icon={<TbReceiptRefund size={55} />}
+        title="Reembolso"
+        description="Garantia de 30 dias em todos os produtos"
+      />
+    </section>
+  )
+}
