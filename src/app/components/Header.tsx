573a7932d900e0a9b3bf495c3feacc103836f3f0
Edu <prog.syrius@gmail.com>
2023-05-07 14:36:27 -0300

    feat: add navigation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image'
 import Logo from '../../assets/images/Logo.svg'
 import Navbar from './Navbar'
-import { BsSearch } from 'react-icons/bs'
 import SearchInput from './SearchInput'
+import Link from 'next/link'
+
 
 export default function Header() {
   return (
     <header className="text-primary font-sans">
       <div className="font-serif flex justify-between items-center px-9  pt-2">
-        <Image src={Logo} width={120} height={120} alt="logo" />
+        <Link href='/'>
+        <Image src={Logo} width={120} height={120} alt="logo" className='cursor-pointer'/>
+        </Link>
         <SearchInput />
         <div className="font-extralight">
           <a
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,41 +1,22 @@
-'use client'
 import { HiOutlineShoppingCart } from 'react-icons/hi'
-import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io'
-import Dropdown from './Dropdown'
-import { useState } from 'react'
+import Link from 'next/link'
 
 export default function Navbar() {
-  const [active, setActive] = useState(false)
-  const handleDropdown = (state: boolean) => {
-    setActive(!state)
-  }
   return (
-    <nav className="px-9 select-none pb-5 border-b border-primary/10">
-      <ul className="flex justify-between">
-        <li>
-          <a href="#">Explorar</a>
-        </li>
-        <li
-          onClick={() => handleDropdown(active)}
-          className="relative flex items-center gap-x-1 cursor-pointer">
-          Categorias {active ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
-          <Dropdown
-            active={active}
-            options={['Masculino', 'Feminino', 'Acessórios', 'Tênis', 'Bolas']}
-          />
-        </li>
-
-        <li>
-          <a href="#">Sobre nós</a>
-        </li>
-        <li>
-          <a href="#">Contato</a>
-        </li>
-        <li className="flex items-center gap-x-1 cursor-pointer">
+    <nav className="px-9 select-none pb-5 border-b border-primary/10 flex justify-between">
+        <Link  href="/explore">
+          Explorar        
+        </Link>
+        <Link href="/about">
+          Sobre nós
+        </Link>
+        <Link href="/contact">
+         Contato
+        </Link>
+        <button className="flex items-center gap-x-1 cursor-pointer">
           <HiOutlineShoppingCart size={22} />
           <span>1</span>
-        </li>
-      </ul>
+        </button>
     </nav>
   )
 }
