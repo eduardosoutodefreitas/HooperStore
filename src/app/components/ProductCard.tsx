7494cfdd02eab3a54f9ec609ee9f05e617b2ef3f
Edu <prog.syrius@gmail.com>
2023-05-07 14:37:30 -0300

    feat: add ProductCard

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,39 @@
+import { BsCartPlusFill } from "react-icons/bs"
+import Product from "../types/Product.types"
+import Image from 'next/image'
+
+interface ProductCard {
+    product:Product
+}
+const ProductCard = ({product}:ProductCard) => {
+  return (
+    <div className="min-w-[250px] h-[400px] max-w-[300px] flex flex-col flex-1 shadow-sm">
+      <div className="relative w-full h-4/5 rounded-sm">
+        <Image src={product.imageUrl} alt={`Product ${product.id} Image`} fill className="rounded-t-sm"/>
+      </div>
+      <div className="flex justify-between px-1">
+        <div>
+           <p className="font-semibold my-2">
+            {product.name}
+        </p>
+        <span className="font-ligth inline-block text-lg">
+            R${product.price}
+        </span>
+        <span className="font-ligth inline-block text-primary/70 line-through text-sm ml-1">
+            R${product.oldPrice}
+        </span>
+        </div>
+        <div className="flex flex-col items-end">
+        <BsCartPlusFill color="Black" size={22} className="mt-2 mb-3 cursor-pointer"/> 
+        <span className="p-1 px-2 rounded-3xl bg-primary text-white font-semibold text-xs">{product.tag}</span>
+        </div>
+       
+      </div>
+        
+
+    </div>
+    
+  )
+}
+
+export default ProductCard
