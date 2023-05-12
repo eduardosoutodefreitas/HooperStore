538ed50d6181f935c7485327341cd834e915a22a
Edu <prog.syrius@gmail.com>
2023-05-12 09:56:12 -0300

    styles: change Service Component and Services Section Styles

--- src/app/components/Service.tsx
+++ src/app/components/Service.tsx
@@ -7,10 +7,10 @@ interface ServiceProps {
 }
 export default function Service({ icon, title, description }: ServiceProps) {
   return (
-    <div className="text-center flex flex-col justify-center items-center gap-y-3">
+    <div className="text-center grid place-items-center flex-1 min-w-[200px] rounded-sm">
       {icon}
-      <h3 className="text-lg">{title}</h3>
-      <p className="text-sm w-2/3">{description}</p>
+      <h3 className="text-lg font-medium pt-5 pb-2">{title}</h3>
+      <p className="text-sm">{description}</p>
     </div>
   )
 }
--- src/app/sections/Services.tsx
+++ src/app/sections/Services.tsx
@@ -8,27 +8,29 @@ import {
 
 export default function Services() {
   return (
-    <section className="my-16 flex justify-between items-center p-5 py-7 bg-primary/5">
-      <Service
-        icon={<BiSupport size={55} />}
-        title="Suporte"
-        description="Atendimento ágil 24/7"
-      />
-      <Service
-        icon={<TbTruckDelivery size={55} />}
-        title="Entrega"
-        description="Frete grátis em compras acima R$100,00"
-      />
-      <Service
-        icon={<TbShoppingCartDiscount size={55} />}
-        title="Descontos"
-        description="Ofertas diárias de até 70% off"
-      />
-      <Service
-        icon={<TbReceiptRefund size={55} />}
-        title="Reembolso"
-        description="Garantia de 30 dias em todos os produtos"
-      />
+    <section className="text-center py-10 bg-primary/10">
+      <div className="px-12 flex flex-wrap justify-between gap-8">
+        <Service
+          icon={<BiSupport size={55} color="rgb(32, 32, 32)" />}
+          title="Suporte"
+          description="Atendimento ágil 24/7"
+        />
+        <Service
+          icon={<TbTruckDelivery size={55} color="rgb(32, 32, 32)" />}
+          title="Entrega"
+          description="Prazo máximo de 7 dias"
+        />
+        <Service
+          icon={<TbShoppingCartDiscount size={55} color="rgb(32, 32, 32)" />}
+          title="Descontos"
+          description="Preços imbatíveis "
+        />
+        <Service
+          icon={<TbReceiptRefund size={55} color="rgb(32, 32, 32)" />}
+          title="Reembolso"
+          description="Garantia de 30 dias"
+        />
+      </div>
     </section>
   )
 }
