5be979f3e9ff57999481feb1441b3bbc3b1f5513
Edu <prog.syrius@gmail.com>
2023-05-12 09:49:50 -0300

    feat: change Header Component styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,30 +1,50 @@
+'use client'
 import Image from 'next/image'
 import Logo from '../../assets/images/Logo.svg'
-import Navbar from './Navbar'
 import SearchInput from './SearchInput'
 import Link from 'next/link'
-
+import { useCartStore } from '../store/cart'
+import { HiOutlineShoppingCart } from 'react-icons/hi'
 
 export default function Header() {
+  const { toggleCart, showCart } = useCartStore()
+  const handleCartClick = () => {
+    toggleCart(showCart)
+  }
   return (
-    <header className="text-primary font-sans">
-      <div className="font-serif flex justify-between items-center px-9  pt-2">
-        <Link href='/'>
-        <Image src={Logo} width={120} height={120} alt="logo" className='cursor-pointer'/>
-        </Link>
-        <SearchInput />
-        <div className="font-extralight">
-          <a
-            href="/"
-            className="pr-5 border-r border-lightGray hover:font-light">
-            login
-          </a>
-          <a href="/" className="pl-5 hover:font-light">
-            criar conta
-          </a>
+    <header className="text-primary shadow-md px-12 sticky top-0 z-30 bg-white ">
+      <div className="flex items-center pt-2">
+        <div>
+          <Link href="/">
+            <Image
+              src={Logo}
+              width={120}
+              height={120}
+              alt="logo"
+              className="cursor-pointer"
+            />
+          </Link>
+        </div>
+        <nav className="flex-1 flex justify-center items-center gap-24">
+          <Link className="text-lg" href="/explore">
+            Explorar
+          </Link>
+          <Link className="text-lg" href="/about">
+            Sobre nós
+          </Link>
+          <Link className="text-lg" href="/contact">
+            Contato
+          </Link>
+        </nav>
+        <div className="flex items-center gap-x-10 flex-2">
+          <SearchInput />
+          <HiOutlineShoppingCart
+            size={22}
+            onClick={handleCartClick}
+            className="cursor-pointer"
+          />
         </div>
       </div>
-      <Navbar />
     </header>
   )
 }
