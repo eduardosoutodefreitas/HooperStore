2c875844a509f9d576ff745187f33dc795b23bc7
Edu <prog.syrius@gmail.com>
2023-05-12 09:52:33 -0300

    style: change Footer Component styles

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,63 +1,61 @@
+// TODO: Adicionar links
+// TODO: Adicionar responsividade
+// TODO: Deixar mais bonito
+import Image from 'next/image'
+import Logo from '../../assets/images/Logo.svg'
+import Link from 'next/link'
+import { FaGithubAlt, FaLinkedin } from 'react-icons/fa'
+import { HiPhone, HiLocationMarker } from 'react-icons/hi'
+import { useCategoryStore } from '../store/category'
+
 export default function Footer() {
-    return (
-      <footer className="bg-primary px-9  text-white">
-        <div className="flex justify-between items-center py-3">
-          <div className="flex flex-col">
-            <h3 className="font-semibold">Informações</h3>
-            <a href="#" target="_blank">
-              Sobre Nós
-            </a>
-            <a href="#" target="_blank">
-              Contato
-            </a>
-            <a href="#" target="_blank">
-              Entrega
-            </a>
-            <a href="#" target="_blank">
-              Política de Privacidade
-            </a>
-            <a href="#" target="_blank">
-              Termos e Condições
-            </a>
-          </div>
-          <div className="flex flex-col">
-            <h3 className="font-semibold">Informações</h3>
-            <a href="#" target="_blank">
-              Sobre Nós
-            </a>
-            <a href="#" target="_blank">
-              Contato
-            </a>
-            <a href="#" target="_blank">
-              Entrega
-            </a>
-            <a href="#" target="_blank">
-              Política de Privacidade
-            </a>
-            <a href="#" target="_blank">
-              Termos e Condições
-            </a>
-          </div>
-          <div className="flex flex-col">
-            <h3 className="font-semibold">Informações</h3>
-            <a href="#" target="_blank">
-              Sobre Nós
-            </a>
-            <a href="#" target="_blank">
-              Contato
-            </a>
-            <a href="#" target="_blank">
-              Entrega
-            </a>
-            <a href="#" target="_blank">
-              Política de Privacidade
-            </a>
-            <a href="#" target="_blank">
-              Termos e Condições
-            </a>
-          </div>
+  const { categories } = useCategoryStore.getState().state
+  return (
+    <footer className="bg-primary text-white px-12">
+      <div className="flex justify-around items-start py-12 border-b border-b-white">
+        <div className="flex flex-col gap-1">
+          <h2 className="font-special font-bold text-4xl">Hooper Store</h2>
+          <Link className="text-lg" href="/explore">
+            Explorar
+          </Link>
+          <Link className="text-lg" href="/about">
+            Sobre nós
+          </Link>
+          <Link className="text-lg" href="/contact">
+            Contato
+          </Link>
         </div>
-        <hr />
+        <div className="flex flex-col-reverse gap-1">
+          {categories.map((category) => (
+            <Link
+              className="text-lg"
+              key={category.id}
+              href={`/explore/${category.id}`}>
+              {category.displayName}
+            </Link>
+          ))}
+        </div>
+        <div className="flex flex-col">
+          <p>
+            <HiPhone size={22} color="white" className="mr-2 inline-block" />
+            +55 21 94002 8922
+          </p>
+
+          <p>
+            <HiLocationMarker
+              size={22}
+              color="white"
+              className="mr-2 inline-block"
+            />{' '}
+            Rua Fictícia, Av.Imaginada
+          </p>
+        </div>
+      </div>
+      <div className="py-12">
+        <div className="flex justify-center items-center gap-5">
+          <FaGithubAlt color="white" size={24} />
+          <FaLinkedin color="white" size={24} />
+        </div>
         <p className="py-3 text-center">
           Feito por{' '}
           <a
@@ -67,7 +65,7 @@ export default function Footer() {
             Eduardo Souto
           </a>
         </p>
-      </footer>
-    )
-  }
-  
+      </div>
+    </footer>
+  )
+}
