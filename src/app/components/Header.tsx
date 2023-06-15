c17c57c77b2c1f0d8df6a37295afaa62c9a67197
Edu <prog.syrius@gmail.com>
2023-06-15 10:32:27 -0300

    style: change Header styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Image from 'next/image'
 import Logo from '../../assets/images/Logo.svg'
-import SearchInput from './SearchInput'
 import Link from 'next/link'
 import { useCartStore } from '../store/cart'
 import { HiOutlineShoppingCart } from 'react-icons/hi'
@@ -13,7 +12,7 @@ export default function Header() {
   }
   return (
     <header className="text-primary shadow-md px-12 sticky top-0 z-30 bg-white ">
-      <div className="flex items-center pt-2">
+      <div className="flex items-center justify-between pt-2">
         <div>
           <Link href="/">
             <Image
@@ -25,25 +24,17 @@ export default function Header() {
             />
           </Link>
         </div>
-        <nav className="flex-1 flex justify-center items-center gap-24">
-          <Link className="text-lg" href="/explore">
-            Explorar
-          </Link>
-          <Link className="text-lg" href="/about">
-            Sobre nós
-          </Link>
-          <Link className="text-lg" href="/contact">
-            Contato
-          </Link>
-        </nav>
-        <div className="flex items-center gap-x-10 flex-2">
-          <SearchInput />
+        <nav className="flex w-1/3 items-center justify-between text-lg">
+          <Link href="/">Home</Link>
+          <Link href="/explore">Explorar</Link>
+          <Link href="/about">Sobre nós</Link>
+          <Link href="/contact">Contato</Link>
           <HiOutlineShoppingCart
             size={22}
             onClick={handleCartClick}
             className="cursor-pointer"
           />
-        </div>
+        </nav>
       </div>
     </header>
   )
