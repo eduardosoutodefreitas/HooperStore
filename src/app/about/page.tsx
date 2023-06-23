0998254cef8ab6d27c8114d73e45e6d1f990915f
Edu <prog.syrius@gmail.com>
2023-06-23 14:10:41 -0300

    feat: add About Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,48 +1,59 @@
 import { SectionTitle } from '../components/SpecialText'
-import { FiGithub, FiLinkedin } from 'react-icons/fi'
-import Image from 'next/image'
-import About from '../../assets/images/About.jpg'
+import { AiFillInstagram, AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
 import Cart from '../components/Cart'
 
 export default function AboutPage() {
   return (
     <>
-      <section className=" px-12 py-24 flex justify-center gap-x-16 items-center">
-        <div className="">
-          <SectionTitle
-            title="Bem-vindo à Hooper Store!"
-            className="rounded-sm"
-          />
-          <p className="my-5">
-            A Hooper Store é uma loja fictícia feita por{' '}
-            <a
-              className="font-bold"
-              href="https://github.com/Hey-Edu"
-              target="_blank">
-              mim
-            </a>
-            . A ideia surgiu da minha paixão pelo Basquete e vontade de criar um
-            projeto que pudesse mostrar meus conhecimentos enquanto resolveria
-            um problema.
-          </p>
-          <p>
-            Nesta loja os produtos possuem definitivamente o melhor preço do
-            mercado, de modo que qualquer atleta em qualquer parte do mundo pode
-            adquirir produtos de qualidade.{' '}
-          </p>
-          <div className="flex items-center mt-5">
-            <FiGithub
+      <section className="min-h-[85vh] p-12 flex flex-col justify-center items-center gap-5">
+        <SectionTitle
+          title="Bem-vindo à Hooper Store!"
+          className="rounded-sm"
+        />
+        <p>
+          A Hooper Store é uma loja fictícia feita por{' '}
+          <a
+            className="font-bold"
+            href="https://github.com/eduardosoutodefreitas"
+            target="_blank">
+            mim
+          </a>
+          . A ideia surgiu da minha paixão pelo Basquete e vontade de criar um
+          projeto que pudesse mostrar meus conhecimentos enquanto resolveria um
+          problema.
+        </p>
+        <p>
+          Nesta loja os produtos possuem definitivamente o melhor preço do
+          mercado, de modo que qualquer atleta em qualquer parte do mundo pode
+          adquirir produtos de qualidade.{' '}
+        </p>
+        <div className="flex items-center min-w-[200px] w-1/4 justify-between">
+          <a href="https://instagram.com/freitas.dev" target="_blank">
+            <AiFillInstagram
               size={35}
               color="#202020"
-              className="mr-10 cursor-pointer"
+              className="cursor-pointer"
             />
-            <FiLinkedin size={35} color="#202020" className="cursor-pointer" />
-          </div>
+          </a>
+          <a href="https://github.com/eduardosoutodefreitas" target="_blank">
+            <AiFillGithub
+              size={35}
+              color="#202020"
+              className="cursor-pointer"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/eduardo-souto-72493b26b/"
+            target="_blank">
+            <AiFillLinkedin
+              size={35}
+              color="#202020"
+              className="cursor-pointer"
+            />
+          </a>
         </div>
-
-        <Image src={About} height={250} width={500} alt="About Image" />
       </section>
-      <Cart/>
+      <Cart />
     </>
   )
 }
